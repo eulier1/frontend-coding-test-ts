6c788eaa3016c9f974037044535759f21c7704ff
Eulier <eulier@toolboxos.com>
2024-01-04 20:25:01 -0300

    feat: add error view

---
+++ src/components/ErrorView.vue
@@ -0,0 +1,19 @@
+<template>
+  <a
+    href="/"
+    class="block max-w-sm p-6 mt-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+  >
+    <h5
+      class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+    >
+      There was an error
+    </h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">
+      But don't worry click on the card to reload the page
+    </p>
+  </a>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
