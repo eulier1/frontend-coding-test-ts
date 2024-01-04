97f086dd5a2426e89dea7a46009b15ba1cde3335
Eulier <eulier@toolboxos.com>
2024-01-04 10:47:42 -0300

    feat: add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './styles/main.css'
 import App from './App.vue'
 import initializeRouter from './router'
 
+const pinia = createPinia()
 const app = createApp(App)
 
 initializeRouter(app)
 
+app.use(pinia)
 app.mount('#app')
